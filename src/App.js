018a5acdf53baaf8ec5dc5c2bb95f4477ea4f6ff
heronmedeiros <heron.medeiros@gmail.com>
2020-07-21 10:12:28 -0300

    Pass data throught state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Tabela from './Tabela'
 
 import './App.css';
+import { render } from '@testing-library/react';
 
-const  autores =  [
-  {
-    nome: 'Meng',
-    livro: 'React',
-    preco: '1000'
-  },
-  {
-    nome: 'Hai',
-    livro: 'Java',
-    preco: '99'
-  },
-  {
-    nome: 'Long',
-    livro: 'Design',
-    preco: '150'
-  },
-  {
-    nome: 'Lee',
-    livro: 'DevOps',
-    preco: '100'
+
+
+class App extends Component {
+ state = {
+    autores:  [
+      {
+        nome: 'Meng',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Hai',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Long',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Lee',
+        livro: 'DevOps',
+        preco: '100'
+      },
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores = { this.state.autores }/>
+      </div>
+    );
   }
-];
 
-function App() {
-  return (
-    <div className="App">
-      <Tabela autores = { autores }/>
-    </div>
-  );
 }
 
 export default App;
