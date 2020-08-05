cddf9bd9778f9728094bec663b9a0495f88a50b7
heronmedeiros <heron.medeiros@gmail.com>
2020-08-05 11:08:12 -0300

    added live site

---
+++ build/precache-manifest.5340be8bd67236926d235722b6e0c9dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4301a50406b42fee1ada95d65ec90f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d621dafa8aecbcd6c50a",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "dcf8a7a1029009e75273",
+    "url": "/static/css/main.53e33a2f.chunk.css"
+  },
+  {
+    "revision": "d621dafa8aecbcd6c50a",
+    "url": "/static/js/2.ffe17cbe.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.ffe17cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf8a7a1029009e75273",
+    "url": "/static/js/main.3dda26fa.chunk.js"
+  },
+  {
+    "revision": "6bced0f77a7cecf20065",
+    "url": "/static/js/runtime-main.2e6b9aa2.js"
+  }
+]);
