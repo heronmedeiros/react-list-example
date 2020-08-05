9e5cc547c0785982b5484461d7cff83a3ef37032
heronmedeiros <heron.medeiros@gmail.com>
2020-08-05 11:58:37 -0300

    Updates

---
+++ precache-manifest.c021feef7b46656f7f34dd0dd786e050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692bf4566c12503d5c8458b9139297ab",
+    "url": "/react-list-example/index.html"
+  },
+  {
+    "revision": "a74c4d84b875df5d90a2",
+    "url": "/react-list-example/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "46f70d862596b7e698b3",
+    "url": "/react-list-example/static/css/main.53e33a2f.chunk.css"
+  },
+  {
+    "revision": "a74c4d84b875df5d90a2",
+    "url": "/react-list-example/static/js/2.750d4807.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/react-list-example/static/js/2.750d4807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f70d862596b7e698b3",
+    "url": "/react-list-example/static/js/main.015b5aa4.chunk.js"
+  },
+  {
+    "revision": "d6d150694640e57e1d8c",
+    "url": "/react-list-example/static/js/runtime-main.cd7a994d.js"
+  }
+]);
